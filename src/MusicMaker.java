3ddabf6173c7526a92ca031e1cf8e23c6a2d10c2
david <davidics613@gmail.com>
2015-02-13 13:12:06 -1000

    Added javadocs.

--- src/MusicMaker.java
+++ src/MusicMaker.java
@@ -3,20 +3,33 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
+/*
+ * TODO:
+ * 
+ * Update code with method to import random patterns.
+ * Remove constants from code.
+ * 
+ */
+
+/*
+ * Cited Sources:
+ * 
+ * http://stackoverflow.com/questions/7782721/java-raw-audio-output/7782749#7782749
+ * http://stackoverflow.com/questions/2064066/does-java-have-built-in-libraries-for-audio-synthesis/2065693#2065693
+ */
+
 /**
- * Generates random music with Java Sound API
- * @author david
- *
+ * Generates random music with Java Sound API.
+ * @author David Neely
  */
 public class MusicMaker {
 
 	/**
-	 * Generates random music
-	 * @param args
+	 * Plays a scale of music starting at A.
+	 * @param args The arguments. Not implemented.
 	 */	
 	public static void main(String[] args) throws LineUnavailableException {
-        final AudioFormat af =
-            new AudioFormat(Note.SAMPLE_RATE, 8, 1, true, true);
+        final AudioFormat af = new AudioFormat(Note.SAMPLE_RATE, 8, 1, true, true);
         SourceDataLine line = AudioSystem.getSourceDataLine(af);
         line.open(af, Note.SAMPLE_RATE);
         line.start();
@@ -27,7 +40,12 @@ public class MusicMaker {
         line.drain();
         line.close();
     }
-
+	/**
+	 * Plays the melody.
+	 * @param line The entire melody to be played.
+	 * @param note The note to be added to the melody.
+	 * @param ms The duration of the note.
+	 */
     private static void play(SourceDataLine line, Note note, int ms) {
         ms = Math.min(ms, Note.SECONDS * 1000);
         int length = Note.SAMPLE_RATE * ms / 1000;
@@ -35,7 +53,11 @@ public class MusicMaker {
     }
 
 }
-
+/**
+ * Lists out the notes to be played.
+ * @author David Neely
+ *
+ */
 enum Note {
 
     REST, A4, A4$, B4, C4, C4$, D4, D4$, E4, F4, F4$, G4, G4$, A5;
@@ -56,10 +78,11 @@ enum Note {
         }
     }
 
+    /**
+     * The individual note to be played.
+     * @return The sin wave.
+     */
     public byte[] data() {
         return sin;
     }
 }
-
-// cited source: http://stackoverflow.com/questions/7782721/java-raw-audio-output/7782749#7782749
-// cited source: http://stackoverflow.com/questions/2064066/does-java-have-built-in-libraries-for-audio-synthesis/2065693#2065693
