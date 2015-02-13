f1d813cd7edec9669cc6422e33bc827a71176b40
david <davidics613@gmail.com>
2015-02-13 12:37:10 -1000

    Set up initial project.

---
+++ src/MusicMaker.java
@@ -0,0 +1,18 @@
+/**
+ * Generates random music with Java Sound API
+ * @author david
+ *
+ */
+public class MusicMaker {
+
+	/**
+	 * Generates random music
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World.");
+
+	}
+
+}
