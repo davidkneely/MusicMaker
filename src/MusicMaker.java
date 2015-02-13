d7d827279eff60737dbc78b320b3b6bb6926b11f
david <davidics613@gmail.com>
2015-02-13 13:02:25 -1000

    Found code on stackoverflow. Added functionality to play scale.

--- src/MusicMaker.java
+++ src/MusicMaker.java
@@ -1,3 +1,8 @@
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
 /**
  * Generates random music with Java Sound API
  * @author david
@@ -8,11 +13,53 @@ public class MusicMaker {
 	/**
 	 * Generates random music
 	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World.");
+	 */	
+	public static void main(String[] args) throws LineUnavailableException {
+        final AudioFormat af =
+            new AudioFormat(Note.SAMPLE_RATE, 8, 1, true, true);
+        SourceDataLine line = AudioSystem.getSourceDataLine(af);
+        line.open(af, Note.SAMPLE_RATE);
+        line.start();
+        for  (Note n : Note.values()) {
+            play(line, n, 500);
+            play(line, Note.REST, 10);
+        }
+        line.drain();
+        line.close();
+    }
 
-	}
+    private static void play(SourceDataLine line, Note note, int ms) {
+        ms = Math.min(ms, Note.SECONDS * 1000);
+        int length = Note.SAMPLE_RATE * ms / 1000;
+        int count = line.write(note.data(), 0, length);
+    }
 
 }
+
+enum Note {
+
+    REST, A4, A4$, B4, C4, C4$, D4, D4$, E4, F4, F4$, G4, G4$, A5;
+    public static final int SAMPLE_RATE = 16 * 1024; // ~16KHz
+    public static final int SECONDS = 2;
+    private byte[] sin = new byte[SECONDS * SAMPLE_RATE];
+
+    Note() {
+        int n = this.ordinal();
+        if (n > 0) {
+            double exp = ((double) n - 1) / 12d;
+            double f = 440d * Math.pow(2d, exp);
+            for (int i = 0; i < sin.length; i++) {
+                double period = (double)SAMPLE_RATE / f;
+                double angle = 2.0 * Math.PI * i / period;
+                sin[i] = (byte)(Math.sin(angle) * 127f);
+            }
+        }
+    }
+
+    public byte[] data() {
+        return sin;
+    }
+}
+
+// cited source: http://stackoverflow.com/questions/7782721/java-raw-audio-output/7782749#7782749
+// cited source: http://stackoverflow.com/questions/2064066/does-java-have-built-in-libraries-for-audio-synthesis/2065693#2065693
